6fe81585e31737478177056e147141148eb8eef2
Alexey Kott <alexey.kott@gmail.com>
2017-05-14 20:15:11 +0300

    продолжим-с дома

--- bot.py
+++ bot.py
@@ -3,22 +3,36 @@ import sqlite3 as sqlite
 import re
 import pickle
 import config as cfg
+from peewee import *
+import bot_strings as bs
 
 botname = '@design_order_bot'
 token = '374519038:AAFhDPFU0NMPC46_e08QOqyOz97YhK06rbQ'
 db_name = 'design_order_bot'
 bot = telebot.TeleBot(cfg.token)
 
+db = SqliteDatabase('bot.db')
+
+class User(Model):
+	user_id = IntegerField(unique = True, primary_key = True)
+	username = CharField()
+	step = IntegerField()
+	task = TextField()
+	deadline = CharField()
+	budget = CharField()
+	email = CharField()
+	mobile = CharField()
+
+	class Meta:
+		database = db
+
 current_stage = 0
 
 @bot.message_handler(commands = ['start'])
 def greeting(message):
 	username = message.from_user.username
-	greeting = '''
-		{}, Вас приветствует компания Быстрый Дизайн!
-	Напишите, какой дизайн Вы хотите заказать?
-	'''.format(username)
-	bot.send_message(message.chat.id, greeting)
+	user = User.create(username = message.chat.id, step = 1)
+	bot.send_message(message.chat.id, bs.greeting.format(username))
 
 
 @bot.message_handler(content_types=['text'])
@@ -47,15 +61,7 @@ def update_user(id, stage, task, deadline, budget, email, mobile):
 	conn = sqlite.connect(config.db_name)
 	curr = conn.cursor()
 	query = '''
-	CREATE TABLE IF NOT EXISTS users(
-		`id` INTEGER PRIMARY KEY NOT NULL,
-		`username` VARCHAR(32),
-		`stage` INTEGER,
-		`task` TEXT,
-		`dealine` TEXT,
-		`budget` TEXT,
-		`email` TEXT,
-		`mobile` TEXT
+	ШТЫУК
 	)'''
 	curr.execute(query)
 	conn.commit()
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-cfg.botname = '@design_order_bot'
-cfg.token = '374519038:AAFhDPFU0NMPC46_e08QOqyOz97YhK06rbQ'
-cfg.db_name = 'design_order_bot'
+botname = '@design_order_bot'
+token = '374519038:AAFhDPFU0NMPC46_e08QOqyOz97YhK06rbQ'
+db_name = 'design_order_bot'
