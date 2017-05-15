49b3cb33c26b0c3e4b48a50dc47e82169394932a
Alexey Kott <alexey.kott@gmail.com>
2017-05-15 20:49:35 +0300

    осталось прикрутить отправку заявки на почту

--- bot.py
+++ bot.py
@@ -36,8 +36,13 @@ def init(message):
 	db.create_table(User)
 	user = User.create(user_id = message.chat.id, username = message.chat.username, step = 1)
 
+@bot.message_handler(commands = ['reboot'])
+def reboot(message):
+	user = User.select().where(User.user_id == message.chat.id).get()
+	user.delete_instance()
+	user = User.create(user_id = message.chat.id, username = message.chat.username, step = 1)
+	route(message.chat.id, message, 1)
 
-current_stage = 0
 
 @bot.message_handler(commands = ['start'])
 def greeting(message):
@@ -45,15 +50,14 @@ def greeting(message):
 	sender_id = message.chat.id
 	user = User.select().where(User.user_id == sender_id).get()
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	bot.send_message(sender_id, bs.budget, reply_markup=markup)
-	bot.send_message(message.chat.id, bs.greeting.format(username))
-	user.step = 2
+	bot.send_message(message.chat.id, bs.greeting.format(username), reply_markup=markup)
+	user.step += 1
 	user.save()
 
 def deadline(sender_id, message):
 	user = User.select().where(User.user_id == sender_id).get()
 	user.task = message.text
-	user.step = 3
+	user.step += 1
 	user.save()
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	back_button = types.KeyboardButton(bs.back)
@@ -76,6 +80,9 @@ def budget(sender_id, message):
 		return False
 	dt = datetime.now()
 	final_date = str(day)+' {0} ({1})'.format(months[month], weekdays[dt.isoweekday()])
+	user.deadline = final_date
+	user.step += 1
+	user.save()
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 	budget_min_button = types.KeyboardButton(bs.budget_min)
 	budget_avg_button = types.KeyboardButton(bs.budget_avg)
@@ -86,32 +93,75 @@ def budget(sender_id, message):
 	bot.send_message(sender_id, bs.budget, reply_markup=markup)
 
 def email(sender_id, message):
+	user = User.select().where(User.user_id == sender_id).get()
+	if message.text == '1':
+		message.text = bs.budget_min
+	if message.text == '2':
+		message.text = bs.budget_avg
+	if message.text == '3':
+		message.text = bs.budget_big
+	if message.text == '4':
+		message.text = bs.budget_max
+	user.budget = message.text
+	user.step += 1
+	user.save()
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	back_button = types.KeyboardButton(bs.back)
 	markup.add(back_button)
 	bot.send_message(sender_id, bs.email, reply_markup=markup)
 
 def mobile(sender_id, message):
+	user = User.select().where(User.user_id == sender_id).get()
+	message.text = message.text.strip()
+	if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", message.text):
+		user.email = message.text
+		user.step += 1
+		user.save()
+	else:
+		bot.send_message(sender_id, bs.email_error)
+		return False
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	back_button = types.KeyboardButton(bs.back)
 	markup.add(back_button)
 	bot.send_message(sender_id, bs.mobile, reply_markup=markup)
 
 def rules(sender_id, message):
+	user = User.select().where(User.user_id == sender_id).get()
+	if re.match(r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$', message.text):
+		user.mobile = message.text
+		user.step += 1
+		user.save()
+	else:
+		bot.send_message(sender_id, bs.mobile_error)
+		return False
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	agreement_button = types.KeyboardButton(bs.agreement)
 	back_button = types.KeyboardButton(bs.back)
+	markup.add(agreement_button)
 	markup.add(back_button)
-	bot.send_message(sender_id, bs.rules, reply_markup=markup)
+	bot.send_message(sender_id, bs.rules, reply_markup=markup, parse_mode="Markdown")
+
+def final(sender_id, message):
+	if message.text != bs.agreement:
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		agreement_button = types.KeyboardButton(bs.agreement)
+		back_button = types.KeyboardButton(bs.back)
+		markup.add(agreement_button)
+		markup.add(back_button)
+		bot.send_message(sender_id, bs.rules, reply_markup=markup, parse_mode="Markdown")
+	else:
+		bot.send_message(sender_id, bs.thanks)	
+		
 
 @bot.message_handler(content_types=['text'])
 def reply(message):
 	sender_id = message.chat.id
 	user = User.select().where(User.user_id == sender_id).get()
 	step = user.step
+	print("Step "+str(step))
 	if message.text == bs.back:
-		if step != 0:
-			step = step-1
+		if step > 1:
+			step -= 2
 		user.step = step
 		user.save()
 		route(sender_id, message, step)
@@ -119,7 +169,7 @@ def reply(message):
 	route(sender_id, message, step)
 
 def route(sender_id, message, step):
-	if step == 1 :
+	if step == 0 or step == 1 :
 		greeting(message)
 	if step == 2:
 		deadline(sender_id, message)
@@ -129,6 +179,12 @@ def route(sender_id, message, step):
 		email(sender_id, message)
 	if step == 5:
 		mobile(sender_id, message)
+	if step == 6:
+		rules(sender_id, message)
+	if step == 7:
+		final(sender_id, message)
+
+
 	
 
 
